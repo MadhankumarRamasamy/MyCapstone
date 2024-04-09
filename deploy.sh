6505dcc4c042279fa3f8ccca80a96da3f12b66ac
MadhankumarRamasamy <rmadhankumar10@gmail.com>
2024-04-09 16:15:53 +0000

    Conflict has been updated

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
-if[[$GIT_BRANCH == "origin/main"]]; then
-docker tag myreactapp madhankumarramasamy/prod
-docker push madhankumarramasamy/prod
-elif [[$GIT_BRANCH == "origin/dev"]]; then
+if [[ $GIT_BRANCH == "origin/main" ]]; then
+ docker tag myreactapp madhankumarramasamy/prod
+ docker push madhankumarramasamy/prod
+elif [[ $GIT_BRANCH == "origin/dev" ]]; then
  docker tag myreactapp madhankumarramasamy/dev
  docker push madhankumarramasamy/dev
  fi
