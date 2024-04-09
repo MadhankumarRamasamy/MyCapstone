09a4dfe61b6c865f389e7ce4068fd0d3956aa71b
MadhankumarRamasamy <rmadhankumar10@gmail.com>
2024-04-09 09:16:41 +0000

    Added branches

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 if[[$GIT_BRANCH == "origin/main"]]; then
 docker tag myreactapp madhankumarramasamy/prod
 docker push madhankumarramasamy/prod
-elif [[$GIT_BRANCH == "dev"]]; then
+elif [[$GIT_BRANCH == "origin/dev"]]; then
  docker tag myreactapp madhankumarramasamy/dev
  docker push madhankumarramasamy/dev
  fi
